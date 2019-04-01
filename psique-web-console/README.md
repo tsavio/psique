bb031142d3d74c21c503e6e29f1b2ae7ff4ae179
Thyago <thyago@usto.re>
2019-04-01 11:58:45 -0300

    [Web] Criação do novo front com nebular

--- psique-web-console/README.md
+++ psique-web-console/README.md
@@ -1,6 +1,6 @@
 # PsiqueWebConsole
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.7.
 
 ## Development server
 
--- psique-web-console/src/app/app.module.ts
+++ psique-web-console/src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { PatientService } from './patient/patient.service';
-import { PatientComponent } from './patient/patient.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
-	declarations: [ AppComponent, PatientComponent ],
-	imports: [ BrowserModule, HttpModule ],
-	providers: [ PatientService ],
-	bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbLayoutModule,
+    NbEvaIconsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
