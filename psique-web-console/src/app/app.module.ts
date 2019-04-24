4ad2bbd9c94e708b7ef013f42804757495a70ea0
Gulloris <gulloris@hotmail.com>
2019-04-24 20:48:19 -0300

    botao video chamada

--- psique-web-console/src/app/app.module.ts
+++ psique-web-console/src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { ButtonShowcaseComponent } from './button/button.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbCardModule  } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonShowcaseComponent
   ],
   imports: [
     BrowserModule,
+    NbCardModule,
+    NbButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'corporate' }),
