e97dd1cad05dc2123ec72ea6bf9b6b4b298404a6
Thyago <thyago@usto.re>
2019-11-22 10:46:56 -0300

    [Web] Teste commit

--- web-app/web-console/src/app/config/hangoutKey.js
+++ web-app/web-console/src/app/config/hangoutKey.js
@@ -3,7 +3,7 @@ const hangoutKey =
 {
     fixCall: "https://hangouts.google.com/call/",
     prefixCall: "raaystieqjdojcqw26",  
-    generateKey: "gera 9 letras aleatorias",
+    generateKey: "gera 9 letras aleatorias a",
     sufixUrl :"?no_rd"
 };
 // URL Example https://hangouts.google.com/call/raaystieqjdojcqw26iapnwjvye?no_rd
